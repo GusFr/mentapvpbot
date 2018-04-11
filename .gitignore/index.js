63d74ff2bf3885719ccdd54f4cfcffab4482e20f
GusFr <35657277+GusFr@users.noreply.github.com>
2018-04-11 16:35:57 -0400

    Update index.js

--- .gitignore/index.js
+++ .gitignore/index.js
@@ -1,1 +1,39 @@
+const Discord = require('discord.js')
+const bot = new Discord.Client()
+
+bot.on('ready', function () {
+	bot.user.setActivity('Menta PvP : play.menta-pvp.fr ou www.menta-pvp.fr').catch(console.error)
+})
+
+bot.on('message', function (message) {
+	if (message.content === '/aide') {
+		message.reply('à besoin d\'aide @Staff !')
+
+	}
+	if (message.content === 'Yo') {
+		message.reply('Yo !')
+
+	}
+	if (message.content === 'yo') {
+		message.reply('Yo !')
+
+	}
+	if (message.content === 'Salut') {
+		message.reply('Salut à toi !')
+
+	}
+
+})
+
+bot.on('guildMemberAdd', function (member) {
+  	member.createDM().then(function (channel) {
+  		return channel.send('**Bienvenue du Menta PvP** ' + member.displayName)
+	}).catch(console.error)
+
+
+})
+
+
+bot.login('NDE2MzMzNTYzNzgxMTg1NTU3.Da96Jg.Kdsj384uNqN3y21Y09A3QXNWY4c')
+
 
